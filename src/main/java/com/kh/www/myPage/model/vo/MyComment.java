007162e3393101f861765514547bc62b1214d1e5
tkadyrrn123 <tkadyrrn123@gmail.com>
2020-06-19 21:46:47 +0900

    ㅇ

--- src/main/java/com/kh/www/myPage/model/vo/MyComment.java
+++ src/main/java/com/kh/www/myPage/model/vo/MyComment.java
@@ -64,6 +64,7 @@ public class MyComment {
 	public void setType(String type) {
 		this.type = type;
 	}
+	
 	@Override
 	public String toString() {
 		return "MyComment [replyNo=" + replyNo + ", boardNo=" + boardNo + ", noticeNo=" + noticeNo + ", voteId="
